e43ce7c9c075f5afee831655b4ca0305ccd642cd
Chakyeoungho <gcha792@gmail.com>
2025-09-25 11:18:21 +0900

    system_programming_class

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int size;
+    scanf("%d", &size);
+
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size - i - 1; j++) printf(" ");
+        for (int j = 0; j < i * 2 + 1; j++) printf("*");
+        printf("\n");
+    }
+
+    return 0;
+}
