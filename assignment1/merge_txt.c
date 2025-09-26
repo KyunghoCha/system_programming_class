4a6e543a0bd382bb77c29f2e496646af1a751a96
KyunghoCha <gcha792@gmail.com>
2025-09-26 20:11:03 +0900

    파일 구조 변경

---
+++ assignment1/merge_txt.c
@@ -0,0 +1,3 @@
+//
+// Created by gcha792 on 9/26/25.
+//
---
+++ assignment2/change_permission.c
@@ -0,0 +1,18 @@
+//
+// Created by gcha792 on 9/26/25.
+//
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+    int newmode;
+    newmode = (int)strtoul(argv[1], (char **)NULL, 8);
+    if (chmod(argv[2], newmode) == -1) {
+        perror(argv[2]);
+        exit(1);
+    }
+
+    exit(0);
+}
